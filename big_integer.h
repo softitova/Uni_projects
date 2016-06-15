5c19510c8f1e5a0608ebde1fcb77138467f8341d
softitova <sof_titova@rambler.ru>
2016-06-15 09:47:14 +0300

    Update big_integer.h

--- big_integer.h
+++ big_integer.h
@@ -1,16 +1,13 @@
 #ifndef BIG_INTEGER_H
 #define BIG_INTEGER_H
 
-//#include <gmp.h>
-//#include <iosfwd>
-using namespace std;
+
+
 struct big_integer
 {
     big_integer();
     big_integer(big_integer const& other);
-    big_integer(int new_int);
-    big_integer(unsigned int a);
-    big_integer(unsigned long long new_int);
+    big_integer(int a);
     explicit big_integer(std::string const& str);
     ~big_integer();
 
@@ -48,9 +45,19 @@ struct big_integer
 
     friend std::string to_string(big_integer const& a);
 
+private:
     std::vector <unsigned int> mass;
     int sign;
-private:
+    friend void my_resize(big_integer &a);
+    friend int abs_compare(std::vector<unsigned> const& a, std::vector<unsigned> const& b);
+    big_integer(unsigned long long new_int);
+    big_integer(unsigned int new_int);
+    friend int compare(big_integer const& a, big_integer const& b);
+    friend void multiply(std::vector<unsigned> &res, const std::vector<unsigned> &a, uint64_t b);
+    friend unsigned int binary (const big_integer &dop, const big_integer &second);
+    friend void div_long_short (big_integer &first, int second);
+    friend big_integer double_code(big_integer a);
+    friend big_integer& make_binary_op (big_integer &a, big_integer const &b, int operation);
 };
 
 big_integer operator+(big_integer a, big_integer const& b);
@@ -76,4 +83,4 @@ bool operator>=(big_integer const& a, big_integer const& b);
 std::string to_string(big_integer const& a);
 std::ostream& operator<<(std::ostream& s, big_integer const& a);
 
-#endif
+#endif // BIG_INTEGER_H
