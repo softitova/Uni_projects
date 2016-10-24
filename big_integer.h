1350b4b1597c058114a3668ba8565d68dcf88730
softitova <sof_titova@rambler.ru>
2016-10-24 21:47:20 +0400

    Update big_integer.h

--- big_integer.h
+++ big_integer.h
@@ -1,5 +1,8 @@
 #ifndef BIG_INTEGER_H
 #define BIG_INTEGER_H
+#include <string>
+//#include <vector>
+#include "vector_optimized.h"
 
 
 
@@ -10,41 +13,41 @@ struct big_integer
     big_integer(int a);
     explicit big_integer(std::string const& str);
     ~big_integer();
-
+    
     big_integer& operator=(big_integer const& other);
-
+    
     big_integer& operator+=(big_integer const& rhs);
     big_integer& operator-=(big_integer const& rhs);
     big_integer& operator*=(big_integer const& rhs);
     big_integer& operator/=(big_integer const& rhs);
     big_integer& operator%=(big_integer const& rhs);
-
+    
     big_integer& operator&=(big_integer const& rhs);
     big_integer& operator|=(big_integer const& rhs);
     big_integer& operator^=(big_integer const& rhs);
-
+    
     big_integer& operator<<=(int rhs);
     big_integer& operator>>=(int rhs);
-
+    
     big_integer operator+() const;
     big_integer operator-() const;
     big_integer operator~() const;
-
+    
     big_integer& operator++();
     big_integer operator++(int);
-
+    
     big_integer& operator--();
     big_integer operator--(int);
-
+    
     friend bool operator==(big_integer const& a, big_integer const& b);
     friend bool operator!=(big_integer const& a, big_integer const& b);
     friend bool operator<(big_integer const& a, big_integer const& b);
     friend bool operator>(big_integer const& a, big_integer const& b);
     friend bool operator<=(big_integer const& a, big_integer const& b);
     friend bool operator>=(big_integer const& a, big_integer const& b);
-
+    
     friend std::string to_string(big_integer const& a);
-
+    
 private:
     std::vector <unsigned int> mass;
     int sign;
